0736dc11b536889732987f1f4ccee3d7db492e3a
Vasiluță Mihai-Alexandru <alexv@siluta.ro>
2023-04-12 19:28:06 +0300

    Support for max_subtasks for contest scoring

--- db/psql_schema/011.max_score_options.sql
+++ db/psql_schema/011.max_score_options.sql
@@ -39,3 +39,34 @@ CREATE OR REPLACE VIEW max_score_view (user_id, problem_id, score) AS
         LEFT JOIN max_submission_strat ms_sub ON (ms_sub.user_id = users.id AND ms_sub.problem_id = pbs.id)
         LEFT JOIN sum_subtasks_strat ms_subtask ON (ms_subtask.user_id = users.id AND ms_subtask.problem_id = pbs.id);
 
+CREATE OR REPLACE VIEW contest_submission_subtask_max_scores (problem_id, user_id, subtask_id, contest_id, max_score) AS
+    SELECT MAX(stks.problem_id) as problem_id, stks.user_id, subtask_id, subs.contest_id, MAX(ROUND(COALESCE(final_percentage, 0) * stks.score / 100.0)) max_score
+    FROM submission_subtasks stks INNER JOIN submissions subs ON stks.submission_id = subs.id
+    WHERE subtask_id IS NOT NULL AND subs.contest_id IS NOT NULL GROUP BY stks.user_id, subtask_id, subs.contest_id;
+
+-- TODO: Refactor this like above
+CREATE OR REPLACE VIEW max_score_contest_view (user_id, problem_id, contest_id, score) AS 
+    WITH max_submission_strat AS (
+        SELECT user_id, problem_id, contest_id, MAX(score) AS max_score FROM submissions WHERE contest_id IS NOT NULL GROUP BY user_id, problem_id, contest_id
+    ), sum_subtasks_strat AS (
+        SELECT user_id, problem_id, contest_id, coalesce(SUM(max_score), -1) AS max_score FROM contest_submission_subtask_max_scores GROUP BY user_id, problem_id, contest_id
+    ) SELECT users.user_id user_id, 
+            pbs.problem_id problem_id,
+            users.contest_id contest_id, 
+            CASE WHEN problems.scoring_strategy = 'max_submission' THEN COALESCE(ms_sub.max_score, -1)
+                 WHEN problems.scoring_strategy = 'sum_subtasks'   THEN COALESCE(ms_subtask.max_score::integer, -1)
+                 ELSE -1
+            END score
+    FROM ((contest_problems pbs INNER JOIN contest_registrations users ON users.contest_id = pbs.contest_id) INNER JOIN problems ON pbs.problem_id = problems.id) 
+        LEFT JOIN max_submission_strat ms_sub ON (ms_sub.user_id = users.user_id AND ms_sub.problem_id = pbs.problem_id AND ms_sub.contest_id = users.contest_id)
+        LEFT JOIN sum_subtasks_strat ms_subtask ON (ms_subtask.user_id = users.user_id AND ms_subtask.problem_id = pbs.problem_id AND ms_subtask.contest_id = users.contest_id);
+
+-- Since we now return -1 on no attempt, we must filter it when computing the top view
+CREATE OR REPLACE VIEW contest_top_view
+    AS WITH contest_scores AS (
+        SELECT user_id, contest_id, SUM(score) AS total_score FROM max_score_contest_view WHERE score >= 0 GROUP BY user_id, contest_id
+    ) SELECT users.user_id, users.contest_id, COALESCE(scores.total_score, 0) AS total_score 
+    FROM contest_registrations users LEFT OUTER JOIN contest_scores scores ON users.user_id = scores.user_id AND users.contest_id = scores.contest_id ORDER BY contest_id, total_score DESC, user_id;
+
+
+
