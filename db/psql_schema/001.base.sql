33b54b0c66f48cdd75395a6bb32e778f50e7a8a9
Vasiluță Mihai-Alexandru <alexv@siluta.ro>
2024-04-25 19:35:43 +0300

    Final schema coalesce

--- db/psql_schema/001.base.sql
+++ db/psql_schema/001.base.sql
@@ -575,3 +575,70 @@ CREATE OR REPLACE TRIGGER username_change_inserts
     EXECUTE FUNCTION insert_username_change();
 
 COMMIT;
+
+-- max score logic
+
+
+CREATE OR REPLACE FUNCTION max_score(user_id bigint, problem_id bigint, OUT mscore decimal) AS $$
+DECLARE
+    scoring_strat text;
+BEGIN
+    SELECT scoring_strategy INTO scoring_strat FROM problems WHERE id = $2 LIMIT 1;
+    IF scoring_strat = 'max_submission' OR scoring_strat = 'acm-icpc' THEN
+        SELECT COALESCE(MAX(score), -1) INTO mscore FROM submissions subs 
+            WHERE subs.user_id = $1 AND subs.problem_id = $2;
+    ELSE
+        WITH subtask_max_scores AS (
+            SELECT subtask_id, MAX(computed_score) AS best_score FROM submission_subtasks stks
+                WHERE subtask_id IS NOT NULL 
+                AND stks.user_id = $1 AND stks.problem_id = $2 
+                GROUP BY subtask_id
+        ) SELECT COALESCE(SUM(best_score), -1) INTO mscore FROM subtask_max_scores;
+    END IF;
+END;
+$$ LANGUAGE plpgsql STABLE PARALLEL SAFE;
+
+
+CREATE TABLE IF NOT EXISTS max_scores (
+    user_id bigint NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    problem_id bigint NOT NULL REFERENCES problems(id) ON DELETE CASCADE,
+    score decimal,
+
+    CONSTRAINT unique_max_score_pair UNIQUE (user_id, problem_id)
+);
+
+CREATE INDEX IF NOT EXISTS user_max_scores ON max_scores(user_id);
+-- used for problem statistics
+CREATE INDEX IF NOT EXISTS problem_max_scores ON max_scores(problem_id);
+
+CREATE OR REPLACE FUNCTION reset_max_score(user_id bigint, problem_id bigint) RETURNS void AS $$
+    INSERT INTO max_scores (user_id, problem_id, score) 
+        SELECT $1 AS user_id, $2 AS problem_id, max_score($1, $2) AS score 
+            WHERE EXISTS (SELECT 1 FROM users WHERE id = $1) AND EXISTS (SELECT 1 FROM problems WHERE id = $2) 
+        ON CONFLICT ON CONSTRAINT unique_max_score_pair 
+            DO UPDATE SET score = max_score($1, $2);
+$$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION score_update_handler() RETURNS TRIGGER AS $$
+BEGIN
+    RETURN(SELECT reset_max_score(NEW.user_id, NEW.problem_id));
+END;
+$$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION score_delete_handler() RETURNS TRIGGER AS $$
+BEGIN
+    RETURN(SELECT reset_max_score(OLD.user_id, OLD.problem_id));
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER submission_score_update
+    AFTER INSERT OR UPDATE OF score
+    ON submissions
+    FOR EACH ROW
+    EXECUTE FUNCTION score_update_handler();
+
+CREATE OR REPLACE TRIGGER submission_score_delete
+    AFTER DELETE
+    ON submissions
+    FOR EACH ROW
+    EXECUTE FUNCTION score_delete_handler();
+
